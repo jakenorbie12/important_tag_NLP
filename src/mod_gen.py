cd7cee4f3438861ad282b8a602866f55114e7af0
jakenorbie12 <jakenorbie@gmail.com>
2020-07-06 12:14:20 -0500

    Adjusted imports

--- src/mod_gen.py
+++ src/mod_gen.py
@@ -6,14 +6,8 @@
 
 import pandas as pd
 import numpy as np
-import sklearn as sk
-import matplotlib.pyplot as plt
-import wheel
-import setuptools
-import scipy
 import lightgbm as lgb
 import random
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
