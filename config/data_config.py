5d331bd357b3490734fa9d27ba949d2e66862adf
jakenorbie12 <jakenorbie@gmail.com>
2020-07-06 21:17:39 -0500

    Fixed pathing issues

--- config/data_config.py
+++ config/data_config.py
@@ -1,4 +1,5 @@
 #===============================================================================
 #     Configurations for Data
 #===============================================================================
-DATASET_PATH = "../data/original/Dataset_08-29-2019.txt"
+DATASET_PATH = "./data/original/Dataset_08-29-2019.txt"
+FEATURES_DATASET_PATH = "./data/original/featured_dataset.csv"
