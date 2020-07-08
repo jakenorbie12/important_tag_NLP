f75955dfa550aa5f86eca6b5ca94ff4448787738
jakenorbie12 <jakenorbie@gmail.com>
2020-07-08 11:49:52 -0500

    Updated config files

--- config/data_config.py
+++ config/data_config.py
@@ -2,4 +2,14 @@
 #     Configurations for Data
 #===============================================================================
 DATASET_PATH = "./data/original/Dataset_08-29-2019.txt"
-FEATURES_DATASET_PATH = "./data/original/featured_dataset.csv"
+
+FEATURES_DATASET_PATH = "./data/process/featured_dataset.csv"
+
+DATA_TRAIN_PATH = "./data/process/data_train.txt"
+LABEL_TRAIN_PATH = "./data/process/label_train.txt"
+DATA_TEST_PATH = "./data/process/data_test.txt"
+LABEL_TEST_PATH = "./data/process/label_test.txt"
+
+PRED_DATASET_PATH = "./data/original/Dataset_08-29-2019.txt"
+
+NEW_DATA_PATH = "./data/final/New_Data.csv"
---
+++ config/model_config.py
@@ -0,0 +1,11 @@
+#===============================================================================
+#     Configurations for Model
+#===============================================================================
+PARAMETERS = {}
+#Don't change these!
+PARAMETERS['objective'] = multiclass
+PARAMETERS['num_classes'] = 15
+#Change these!
+PARAMETERS['learning_rate'] = 0.03
+
+MODEL_PATH = './models/model.txt'
